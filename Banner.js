f51adaf3fbc0e4e27b068f4251355ac580b87b31
Rhiscky10 <144527530+Rhiscky10@users.noreply.github.com>
2023-09-21 00:53:19 +0000

    Create Banner.js

---
+++ Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+
+
+
+
+
+
+
+class Banner extends React.Component{
+
+    render() {
+        return <div>
+          <img src="https://wallpapers.com/images/high/4k-laptop-luminous-blue-flower-25a78tme4h8q2qr6.webp"/>
+          
+        
+        </div>;
+    }
+}
+
+
+export default Banner;
